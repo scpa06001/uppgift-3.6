9f2094d9a8019b397d72c0b930ae530e40ba3c67
Admin <Admin@TEB123002>
2023-10-20 15:18:46 +0200

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift_3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt förnamn och efternamn.");
+            string helanamn = Console.ReadLine();
+            int mellanslagIndex = helanamn.IndexOf(" ");
+            string förnamn = helanamn[..mellanslagIndex];
+            string efternamn = helanamn[(mellanslagIndex + 1)..];
+
+            if (förnamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine("Ditt förnamn kommer före ditt efternamn i bokstavsordningen");
+            }
+            else if (förnamn.CompareTo(efternamn) > 0)
+            {
+                Console.WriteLine("Ditt förnamn kommer efter ditt efternamn i bokstavsordningen");
+            }
+            else if (förnamn.CompareTo(efternamn) == 0)
+            {
+                Console.WriteLine("Dina namn är på samma plats i bokstavsordnigen");
+            }
+                Console.ReadKey();
+
+        }
+    }
+}
